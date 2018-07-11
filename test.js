48659db67367734a6fbffeccfa8064df36bdba7a
Siltheu <41127569+Siltheu@users.noreply.github.com>
2018-07-11 21:55:52 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, arguments) => {
+
+    return message.channel.send("de bot werkt nog")
+
+}
+
+module.exports.help = {
+    name: "test"
+}
